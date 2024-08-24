dafdcb6c43c0e058b65c0eff908da61add68e37c
thunyP <tpt8669@gmail.com>
2024-08-24 14:40:32 -0500

    final post

---
+++ Develop/index.js
@@ -0,0 +1,86 @@
+// TODO: Include packages needed for this application
+import inquirer from 'inquirer';
+import fs from 'fs';
+import generateMarkdown from './utils/generateMarkdown.js';
+
+// TODO: Create an array of questions for user input
+const questions = [
+        //title 
+        {
+          type: 'input',
+          name: 'title',
+          message: 'What is your Projects Name?',
+        },
+        //license
+        {
+            type: 'checkbox',
+            name: 'license',
+            message: 'What kind of Liscense is your project using',
+            choices: ['MIT', 'Apache-2.0', 'GPL-3.0', 'BSD-3-Clause', 'None']
+        },
+        //descrtiption
+        {
+          type: 'input',
+          name: 'description',
+          message: 'Write a Short Descirption of your project',
+        },
+        //Usage
+        {
+          type: 'input',
+          name: 'usage',
+          Message: 'How is this application used'
+        },
+        // NPM i
+        {
+          type: 'input',
+          name: 'dependencies',
+          message: 'what command should run to install dependences',
+        },
+        //Test
+        {
+            type: 'input',
+            name: 'test',
+            message: 'what command should run to test dependenies ',
+
+        },
+        //Contact Info
+        //Github
+        {
+          type: 'input',
+          name: 'username',
+          message: 'What is GitHub Username?',
+        },
+        //Email
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address',
+        }
+      ];
+
+// // TODO: Create a function to write README file
+
+function writeToFile(fileName, data) {
+  try {
+      fs.writeFileSync(fileName, data);
+  } catch (err) {
+      console.log(err);
+      console.log("Unable to write to the file.");
+  }
+}
+
+// fs.writeFile(fileName, data, (err) =>
+//   err ? console.error(err) : console.log('Success!')
+// );
+
+// // TODO: Create a function to initialize app
+
+function init() {
+  inquirer
+      .prompt(questions).then((response) =>
+          writeToFile('./product/README.md', generateMarkdown(response))
+      )
+}
+
+// // Function call to initialize app
+init();
---
+++ Develop/utils/generateMarkdown.js
@@ -0,0 +1,83 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+function renderLicenseBadge(license) {
+  switch(license){
+    case "MIT":
+    return '[![MIT License](https://img.shields.io/badge/License-MIT-green.svg)'
+    case "Apache-2.0":
+    return '[![Apache 2.0 License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)'
+    case "GPL-3.0":
+    return '[![GPL v3 License](https://img.shields.io/badge/License-GPLv3-blue.svg)'
+    case "BSD-3-Clause":
+    return '[![BSD 3-Clause License](https://img.shields.io/badge/License-BSD%203--Clause-orange.svg)'
+      // If there is no license, return an empty string
+    case "None":
+    return ''
+  }
+}
+
+//renders links 
+function renderLicenseSection(license) {
+  switch (license){
+
+    case "MIT":
+    return `## License
+    link:'https://opensource.org/license/mit'
+    `
+
+    case "GPL-3.0":
+    return `## License 
+    link: https://www.gnu.org/licenses/gpl-3.0.en.html
+    `
+    
+    case "Apache-2.0":
+    return `## License
+    link: https://apache.org/
+    `
+
+    case "BSD-3-Clause" :
+    return`## License
+    link: https://opensource.org/license/bsd-3-clause
+    `
+    
+    case "None":
+    return `## License 
+    link: `
+  }
+}
+//calls everything from above realting to badge and link of licesne on line 46
+function generateMarkdown(data) {
+  return `# ${data.title}
+${renderLicenseBadge(data.license)}
+
+## Description
+${data.description}
+
+## Table of Contents
+* [Features](#features)
+* [Usage](#usage)
+* [Dependencies](depenedencies)
+* [Testing](#testing)
+* [Questions](#questions)
+
+## Screenshot
+![alt-text](${data.screenshot})
+
+## Usage
+*${data.usage}
+
+## Install
+
+${data.dependencies}
+
+## Testing
+
+${data.test}
+
+## Questions
+### Contact Info
+* email: ${data.email} 
+* [github/${data.username}](https://github.com/${data.username}).
+`;
+}
+
+export default generateMarkdown;
